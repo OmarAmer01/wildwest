f687b6f3953b11e254a4a34087879f6b5098d3fb
OmarAmer01 <62311767+OmarAmer01@users.noreply.github.com>
2020-12-29 17:41:48 +0200

    Rearranged hierarchy

--- functions/PlayerOneAndTwo0.0.3.asm
+++ functions/PlayerOneAndTwo0.0.3.asm
@@ -1,44 +1,6 @@
-
-.model compact
-.stack 64
-.data
-newLine db 13,10 , '$' ;for debugging purposes
-enterPoneName db "Enter Player One Name:" , 13,10 ,'$' ; messages to enter name
-enterPtwoName db "Enter Player Two Name:" , 13,10 ,'$' ; messages to enter name
-p1 db 15,?,30 dup ('$') ; player 1 name
-p2 db 15,?,30 dup ('$') ; player 2 name
-                        ;ATTENTION 
-                        ;WHEN WE PRINT PLAYER NAMES, N2OOL EL OFFSET BTA3HOM PLUS 2
-
-
-time dw ?  ;for bullet to travel
-min db ?
-x dw 10    ;bullet starting pos
-y dw 100
-
-Prompt db "press ","$ " ; for shield
-P1key db ?
-P2key db ?
-P1HasSheild db 0d
-P2HasSheild db 0d
-
-P1HasSheildPrompt db "Player 1 has shield ","$ "
-P2HasSheildPrompt db "Player 2 has shield ","$ "
-
-ArrowsSC db 48h,4Bh,50h,4dh 
-wasdSC   db 77h,61h,73h,64h
-
-left  db  17,"$ "
-right db  16,"$ "
-up    db  24,"$ "
-down  db  25,"$ "
-
-w  db  "w ","$ "
-a  db  "a ","$ "
-s  db  "s ","$ "
-d  db  "d ","$ ";shield end
-
-;The following are Graphics related data
+.MODEL compact
+.STACK 64
+.DATA
 
 imgW equ 68
 imgH equ 102
@@ -49,8 +11,9 @@ Yposition DW 300 ;Player One Y_Position
 XptwoPosition DW 550 ;Player Two X_Position
 YptwoPosition DW 300 ;Player Two Y_Position
 
+;put the img data outputed by python script here:
 ;--------------------------------- Player One --------------------------------------------
-p1Raised DB 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 
+img DB 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 
  DB 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 
  DB 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 
  DB 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 
@@ -227,7 +190,7 @@ p1Raised DB 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
 ;--------------------------------------------------------------------------------
 
 ;------------------------------------Player Two ---------------------------------
-p2Raised DB 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 
+imgTwo DB 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 
  DB 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 
  DB 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 
  DB 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 22, 23, 0, 0, 0, 0 
@@ -401,64 +364,38 @@ p2Raised DB 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
  DB 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 
  DB 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 
  DB 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
-
-;------------------------------------------- END DATA SEGMENT --------------------------------------------------
-
-.code
-main proc far
-
-
-mov ax,@data
-mov ds,ax ; Initiate data segment
-
-call cursorToMiddle   ; Center cursor to display message
-
-call clearScreen ; to clear the screen    
-
-
-
-lea dx,enterPoneName  ; print ENTER PLAYER 1
-mov ah,9
-int 21h
-
-call cursorToMiddle   ; Center cursor to display message
-
-call typeNamePone     ; enter name of p1
-
-call cursorToMiddle
-call clearScreen ; to clear the screen  
-
-
-lea dx,enterPtwoName  ; print ENTER PLAYER 2
-mov ah,9
-int 21h
-
-call typeNamePtwo
-;----------------------------------TAKING NAMES COMPLETED-----------------------------
-
-;---------------------Now we should choose to chat or play a round of wild west™------
-;--------------------b3d ama n5tar b2a tbd2 el l3ba zy keda :
+.CODE
+MAIN PROC FAR
+    MOV AX , @DATA
+    MOV DS , AX
+;---------- Graphics Mode configuration ---------;
 	mov ax, 4F02h 
 	mov bx, 0100h	
-	INT 10h      	;To Graphics Mode
-
-    call colorDesert
-
+	INT 10h      	;execute the configuration
+;--------- if these configurations executed twice else where in the code all previous drawings will be lost ------;
     call DrawPlayerOne    
     call DrawPlayerTwo    
+   
+MAIN ENDP	
 
-; Now we wait a random time then show shield prompt
-mov ax,2c00h
-int 21h         ;Get system time
-                ; 3ayzeen ngeeb meno random value
-
-call ShieldPrompt
-
-
-;call exit ; to exit application
-main endp
-;--------------------------------------- PROCEDURES ----------------------------------
-
+;-----------Clear screen  -----
+Clear_screen PROC
+      mov ax,0600h
+      mov bh,07
+      mov cx,0000
+      mov dx,184Fh  ;to Row=18H col=4Fh (right bottom)
+      int 10H
+      RET
+Clear_screen endp
+;-----------set cursor in screen center -----
+Cursor PROC
+        mov ah,02
+        mov bh,00
+        mov dl,39
+        mov dh,12
+        int 10h
+        RET
+Cursor endp
 
 DrawPlayerOne PROC 
 	       ;mov ax, 4F02h    ;
@@ -469,7 +406,7 @@ DrawPlayerOne PROC
 	       ADD CX, imgW  	;set the width (X) up to 64 (based on image resolution)
 	       MOV DX, Yposition 	;set the hieght (Y) up to 64 (based on image resolution)
 	       ADD DX, imgH 	;set the hieght (Y) up to 64 (based on image resolution)
-		   mov DI, offset p1Raised  ; to iterate over the pixels
+		   mov DI, offset img  ; to iterate over the pixels
 	       jmp Start    	;Avoid drawing before the calculations
 	Drawit:
 	       MOV AH,0Ch   	;set the configuration to writing a pixel
@@ -491,8 +428,6 @@ DrawPlayerOne PROC
 	RET
 DrawPlayerOne ENDP
 
-
-
 DrawPlayerTwo PROC 
 	       ;mov ax, 4F02h    ;
 	       ;mov bx, 0100h    ; 640x400 screen graphics mode
@@ -502,7 +437,7 @@ DrawPlayerTwo PROC
 	       ADD CX, imgW  	;set the width (X) up to 64 (based on image resolution)
 	       MOV DX, YptwoPosition 	;set the hieght (Y) up to 64 (based on image resolution)
 	       ADD DX, imgH 	;set the hieght (Y) up to 64 (based on image resolution)
-		   mov DI, offset p2Raised  ; to iterate over the pixels
+		   mov DI, offset imgTwo  ; to iterate over the pixels
 	       jmp StartTwo    	;Avoid drawing before the calculations
 	DrawitTwo:
 	       MOV AH,0Ch   	;set the configuration to writing a pixel
@@ -524,387 +459,8 @@ DrawPlayerTwo PROC
     RET
 DrawPlayerTwo ENDP
 
+end main 
 
 
 
-exit proc ; Exit the application
-    mov ah,4ch
-    int 21h
-exit endp
-       
-       
-clearScreen proc ; CLEAR SCREEN
-    mov bh,7
-    mov ah,7
-    mov al,0
-    mov cx,0
-    mov dx,184fh
-    int 10h 
-    ret
-clearScreen endp
-
-colorDesert proc ; CLEAR SCREEN
-MOV AH, 06h    ; Scroll up function
-XOR AL, AL     ; Clear entire screen
-XOR CX, CX     ; Upper left corner CH=row, CL=column
-MOV DX, 184FH  ; lower right corner DH=row, DL=column 
-MOV BH, 0E0h  ; YellowOnBlue
-INT 10H
-    ret
-colorDesert endp
-
-showCursor proc
-    mov CX,0607h
-    mov ah,1h
-    int 10h
-    ret
-showCursor endp
-
-cursorToMiddle proc ; moves the cursor to the middle of the screen 
-                    ; VERY IMPORTANT : I DONT KNOW WHY, BUT NEVER USE AFTER CLEAR SCREEN
-                    ; USE BEFORE IT
-                    ; WELNABY YA SHABAB
-    mov cx,0
-    mov ax,0200h
-    mov dx,0720h
-    int 10h
-    ret
-cursorToMiddle endp
-                         
-                         
-                         
-typeNamePone proc ; moves the cursor to the below middle of the screen and takes input [PLAYER ONE]     
-    
-   ; call showCursor
-    mov ax,0200h
-    mov dx,0920h ; Cursor to bellow middle
-    mov bh,0
-    int 10h
-    
-    mov ah,0ah   ; to take input from keyboard and store in p1
-    lea dx,p1
-    int 21h
-    
-    
-    cmp p1,41h
-    jl less
-    jg gr8
-
-    less:mov ah,2
-    mov dl,8
-    ret
-
-    gr8:
-    cmp al,7ah
-    jl lesser
-    jg less
-
-
-    lesser:
-    
-    ret
-typeNamePone endp
-
-typeNamePtwo proc ; moves the cursor to the below middle of the screen and takes input [PLAYER ONE]     
-    
-   ; call showCursor
-    mov ax,0200h
-    mov dx,0920h ; Cursor to bellow middle
-    mov bh,0
-    int 10h
-    
-    mov ah,0ah   ; to take input from keyboard and store in p2
-    lea dx,p2
-    int 21h
-    
-    
-    ret
-typeNamePtwo endp
-
-bullet proc
-
-mov ah,0
-mov al,13h
-int 10h
-
-mov al,15 ;draw pixel initially
-mov cx, x
-mov dx, y
-mov ah,0ch;
-int 10h
-
-mov ah,2ch ; 
-int 21h
-add dx,5
-mov time,dx
-mov min,cl
-
-BulletL:
-mov ah,2ch
-int 21h
-
-cmp cl,min ;when a minute passes reset time
-jz ignore
-mov time,0h
-inc min
-ignore:
-
-cmp dx, time
-jb BulletL ;draw with time
-
-mov al,0
-mov cx, x
-mov dx, y
-mov ah,0ch
-int 10h
-add x,4 ;increase to increase speed
-mov al,15
-mov cx, x
-mov dx, y
-mov ah,0ch
-int 10h
-
-mov ah,2ch
-int 21h
-add dx,5
-mov time,dx
-
-mov cx,x
-cmp cx,200 ;where to the bullet stops
-jbe BulletL
-
-ret
-
-bullet endp
-
-clearkeyboardbuffer		proc	near
-
-	push		ax
-	push		es
-	mov		ax, 0000h
-	mov		es, ax
-	mov		es:[041ah], 041eh
-	mov		es:[041ch], 041eh				; Clears keyboard buffer
-	pop		es
-	pop		ax
-
-	ret
-clearkeyboardbuffer		endp
-
-ShieldPrompt proc
-
-
-
-mov ax,0200h ;set mouse pos
-mov dx,0204h
-int 10h
-
-mov ah,2ch
-int 21h
-mov ax,dx
-mov dx,0
-mov ah,0
-mov bl,4
-div bl      ; do calculations based on system time
-
-
-mov ah,9
-lea dx,Prompt
-int 21h
-
-
-
-
-lea di,ArrowsSC
-lea si,wasdSC
-
-cmp ah,0
-jz pup
-cmp ah,1
-jz pleft
-cmp ah,2
-jz pdown1
-cmp ah,3
-jz pright1
-
-pup: mov ah,9
-lea dx,up
-int 21h
-
-mov ax,0200h ;set mouse pos
-mov dx,0404h
-int 10h
-
-mov ah,9
-lea dx,Prompt
-int 21h
-
-mov ah,9
-lea dx,w
-int 21h
-
-jmp check1
-clr1:call clearkeyboardbuffer
-
-check1:mov ax,0
-mov ah,1h
-int 16h
-jz check1
-
-
-add di,0
-cmp ah, [di]
-jnz skip1
-mov P1HasSheild,1
-mov ah,9
-lea dx,P1HasSheildPrompt
-int 21h
-jmp l1
-skip1:add si,0
-cmp al,[si]
-jnz clr1
-mov P2HasSheild,1
-mov ah,9
-lea dx,P2HasSheildPrompt
-int 21h
-jmp l1
-
-pright1:jmp pright
-pdown1: jmp pdown
-
-pleft: mov ah,9
-lea dx,left
-int 21h
-
-mov ax,0200h ;set mouse pos
-mov dx,0404h
-int 10h
-
-mov ah,9
-lea dx,Prompt
-int 21h
-
-mov ah,9
-lea dx,a
-int 21h
-
-jmp check2
-clr2:call clearkeyboardbuffer
-
-check2: mov ax,0
-mov ah,1h
-int 16h
-jz check2
-
-
-
-;add di,1
-cmp ah,[di+1]
-jnz skip2
-mov P1HasSheild,1
-mov ah,9
-lea dx,P1HasSheildPrompt
-int 21h
-jmp l1
-skip2:;add si,1
-cmp al,[si+1]
-jnz clr2
-mov P2HasSheild,1
-mov ah,9
-lea dx,P2HasSheildPrompt
-int 21h
-jmp l1
-
-
-l1:jmp l
-
-pdown: mov ah,9
-lea dx,down
-int 21h
-
-mov ax,0200h ;set mouse pos
-mov dx,0404h
-int 10h
-
-mov ah,9
-lea dx,Prompt
-int 21h
-
-mov ah,9
-lea dx,s
-int 21h
-
-jmp check3
-clr3:call clearkeyboardbuffer
-
-check3:
-mov ah,1
-int 16h
-jz check3
-
-;add di,2
-cmp ah, [di+2]
-jnz skip3
-mov P1HasSheild,1
-mov ah,9
-lea dx,P1HasSheildPrompt
-int 21h
-jmp l
-skip3:
-cmp al,[si+2]
-jnz clr3
-mov P2HasSheild,1
-mov ah,9
-lea dx,P2HasSheildPrompt
-int 21h
-jmp l
-
-
-
-pright:mov ah,9
-lea dx,right
-int 21h
-
-mov ax,0200h ;set mouse pos
-mov dx,0404h
-int 10h
-
-mov ah,9
-lea dx,Prompt
-int 21h
-
-mov ah,9
-lea dx,d
-int 21h
-
-jmp check4
-clr4:call clearkeyboardbuffer
-
-check4: mov ax,0
-mov ah,1
-int 16h
-jz check4
-
-
-;add di,3
-cmp ah,[di+3]
-jnz skip4
-mov P1HasSheild,1
-mov ah,9
-lea dx,P1HasSheildPrompt
-int 21h
-jmp l
-skip4:;add si,3
-cmp al,[si+3]
-jnz clr4
-mov P2HasSheild,1
-mov ah,9
-lea dx,P2HasSheildPrompt
-int 21h
-jmp l
-
-l:ret
-
-ShieldPrompt endp
-
-end main
+          
