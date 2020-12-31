26b610b3cb07ed7d809d37920e558d199da7d995
Mousa Mohammad Abdulmaxod <mousamax101@gmail.com>
2020-12-31 15:06:58 +0200

    Update ShootMechanic0.1.asm

--- functions/ShootMechanic0.1.asm
+++ functions/ShootMechanic0.1.asm
@@ -19,7 +19,11 @@ YptwoPosition DW 285 ;Player Two Y_Position
 bulletOneXPosition DW 145  ;Bullet One X_Position
 bulletOneYPosition equ 316 ;Bullet One Y_Position
 
-reset DW ?
+bulletTwoXPosition DW 470
+
+reset DW ?  
+
+bgrndcolor db 0           ;Set background color for images  
 
 clearbulletone DB 0       ;Set to one to draw a bullet with the background color 
 ;put the img data outputed by python script here:
@@ -698,6 +702,27 @@ bulletimg DB 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
  DB 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 
  DB 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 
  DB 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
+ ;-----------------------------------------------------------------
+ bulletimg2 DB 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 
+ DB 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 
+ DB 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 
+ DB 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 
+ DB 0, 6, 116, 6, 6, 6, 6, 116, 6, 43, 6, 116, 6, 6, 6, 6, 6, 6, 6, 6, 6, 116, 116, 116, 116, 116, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 
+ DB 6, 116, 6, 6, 6, 6, 116, 6, 43, 6, 116, 6, 6, 6, 6, 6, 6, 6, 6, 6, 116, 116, 116, 116, 116, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 6 
+ DB 116, 6, 6, 6, 6, 116, 6, 43, 6, 116, 6, 6, 6, 6, 6, 6, 6, 6, 6, 116, 116, 116, 116, 116, 116, 116, 116, 116, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 6, 116 
+ DB 6, 6, 6, 6, 116, 6, 43, 6, 116, 6, 6, 6, 6, 6, 6, 6, 6, 6, 116, 116, 116, 116, 116, 116, 116, 116, 116, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 6, 116, 6 
+ DB 6, 6, 6, 116, 6, 43, 6, 116, 6, 6, 6, 6, 6, 6, 6, 6, 6, 116, 116, 116, 116, 116, 116, 116, 116, 116, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 6, 116, 6, 6 
+ DB 6, 6, 116, 6, 43, 6, 116, 6, 6, 6, 6, 6, 6, 6, 6, 6, 116, 116, 116, 116, 116, 116, 116, 116, 116, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 6, 116, 6, 6, 6 
+ DB 6, 116, 6, 43, 6, 116, 6, 6, 6, 6, 6, 6, 6, 6, 6, 116, 116, 116, 116, 116, 116, 116, 116, 116, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 6, 116, 6, 6, 6, 6 
+ DB 116, 6, 43, 6, 116, 6, 6, 6, 6, 6, 6, 6, 6, 6, 116, 116, 116, 116, 116, 116, 116, 116, 116, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 6, 116, 6, 6, 6, 6, 116 
+ DB 6, 43, 6, 116, 6, 6, 6, 6, 6, 6, 6, 6, 6, 116, 116, 116, 116, 116, 116, 116, 116, 116, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 6, 116, 6, 6, 6, 6, 116, 6 
+ DB 43, 6, 116, 6, 6, 6, 6, 6, 6, 6, 6, 6, 116, 116, 116, 116, 116, 116, 116, 116, 116, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 6, 116, 6, 6, 6, 6, 116, 6, 43 
+ DB 6, 116, 6, 6, 6, 6, 6, 6, 6, 6, 6, 116, 116, 116, 116, 116, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 6, 116, 6, 6, 6, 6, 116, 6, 43, 6 
+ DB 116, 6, 6, 6, 6, 6, 6, 6, 6, 6, 116, 116, 116, 116, 116, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 
+ DB 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 
+ DB 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 
+ DB 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 
+ DB 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
 .CODE
 MAIN PROC FAR
     MOV AX , @DATA
@@ -708,15 +733,14 @@ MAIN PROC FAR
 	INT 10h      	;execute the configuration
 ;--------- if these configurations executed twice else where in the code all previous drawings will be lost ------; 
     call DrawPlayerOne
-    call DrawPlayerTwo
-    
+    call DrawPlayerTwo    
 	;--------- Wait for testing ----------
 	testing:mov ah, 07h   ;Input from keyboard without echo to the screen
 	int 21h
-    call ShootPlayerTwo
+    call ShootPlayerOne
     jmp testing
 MAIN ENDP	
-;-------- Player will shoot player two --------;
+;-------- Player one will shoot player two --------;
 ShootPlayerTwo PROC
     mov bx,bulletOneXPosition    ;save the original value after shootting 
     mov reset,bx
@@ -736,6 +760,26 @@ ShootPlayerTwo PROC
         mov bulletOneXPosition,bx
     RET
 ShootPlayerTwo ENDP   
+;-------- Player two will shoot player one --------;
+ShootPlayerOne PROC
+    mov bx,bulletTwoXPosition   ;save the original value after shootting 
+    mov reset,bx
+    movebullet2:
+        Call DrawPlayerTwoBullet
+        ;Delay
+        ;Clear Previous shot
+        mov clearbulletone,1
+        call DrawPlayerTwoBullet
+        mov clearbulletone,0
+        ;---------------------
+        SUB bulletTwoXPosition,10        
+        CMP bulletTwoXPosition,140
+        JNZ movebullet2      	   
+        ;---------Reset the start position-----
+        mov bx,reset
+        mov bulletTwoXPosition,bx
+    RET
+ShootPlayerOne ENDP   
 DrawPlayerOneBullet PROC 
 	       ;mov ax, 4F02h    ;
 	       ;mov bx, 0100h    ; 640x400 screen graphics mode
@@ -751,7 +795,7 @@ DrawPlayerOneBullet PROC
 	       MOV AH,0Ch   	;set the configuration to writing a pixel
            cmp clearbulletone,1
            JNZ currentcolor 
-           mov al, 0        ; color of Background
+           mov al, bgrndcolor        ; color of Background
            MOV BH,00h   	;set the page number
 	       INT 10h      	;execute the configuration
            jmp StartBullet
@@ -834,6 +878,43 @@ DrawPlayerTwo PROC
 	ENDINGTwo:
     RET
 DrawPlayerTwo ENDP
+
+DrawPlayerTwoBullet PROC 
+	       ;mov ax, 4F02h    ;
+	       ;mov bx, 0100h    ; 640x400 screen graphics mode
+	       ;INT 10h      	;execute the configuration
+	       MOV AH,0Bh   	;set the configuration
+	       MOV CX, bulletTwoXPosition  	;set the start drawing point 
+	       ADD CX, bulletimgW  	;set the width (X) up to 64 (based on image resolution)
+	       MOV DX, bulletOneYPosition 	;set the hieght (Y) up to 64 (based on image resolution)
+	       ADD DX, bulletimgH 	;set the hieght (Y) up to 64 (based on image resolution)
+		   mov DI, offset bulletimg2  ; to iterate over the pixels
+	       jmp StartBullet2    	;Avoid drawing before the calculations
+	DrawitBullet2:
+	       MOV AH,0Ch   	;set the configuration to writing a pixel
+           cmp clearbulletone,1
+           JNZ currentcolor2 
+           mov al, bgrndcolor        ; color of Background
+           MOV BH,00h   	;set the page number
+	       INT 10h      	;execute the configuration
+           jmp StartBullet2
+    currentcolor2: mov al, [DI]      ; color of the current coordinates 
+	       MOV BH,00h   	;set the page number
+	       INT 10h      	;execute the configuration
+	StartBullet2: 
+		   inc DI
+	       DEC Cx       	;  loop iteration in x direction
+	       cmp Cx,bulletTwoXPosition         ;JNZ Drawit      	;  check if we can draw c urrent x and y and excape the y iteration
+	       JNZ DrawitBullet2
+           ADD Cx, bulletimgW 	;  if loop iteration in y direction, then x should start over so that we sweep the grid
+	       DEC DX       	;  loop iteration in y direction
+	       cmp DX,bulletOneYPosition
+           JZ  ENDINGBullet2   	;  both x and y reached 00 so end program
+		   Jmp DrawitBullet2
+
+	ENDINGBullet2:
+	RET
+DrawPlayerTwoBullet ENDP
 
 
 end main 
